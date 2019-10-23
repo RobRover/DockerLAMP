b7b4c728e55cbcd80fddd96a21afd2b0fe2711ec
rgarciat <robert.garcia03@estudiant.upf.edu>
2019-10-23 04:01:39 -0400

    start project working

---
+++ web2/index.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'db2';
+$user = 'root';
+$password = 'root';
+$db = 'db2';
+
+$conn = new mysqli($host,$user,$password,$db);
+if($conn->connect_error){
+	echo 'connection failed' . $conn->connect_error;
+}
+echo 'Successfully connected to MYSQL';
+
+?>
